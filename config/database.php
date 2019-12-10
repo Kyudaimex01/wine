12d35b55e2dcacaeb11a1fb7c4372d05a8050538
wilson choque <wilson.choque.c@gmail.com>
2019-12-10 13:47:47 -0400

    added pgsql credentials

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$DATABASE_URL= parse_url('postgres://qvnztqztkimzwa:7542986acef51e206d9beb8f07481b2a2d45d50a05a89ab704b5ff651fad34c9@ec2-23-21-249-0.compute-1.amazonaws.com:5432/dado1vepbdghlm');
 return [
 
     /*
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,13 +56,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
